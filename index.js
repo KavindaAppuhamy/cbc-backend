bdef3e98f571ca2db6b09d6315a9c10fe5439034
Kavinda <kavinda.appuhamy@gmail.com>
2025-05-29 14:28:19 +0530

    env applied for hide MongoDB URL ✏️

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import userRouter from './routes/userRouter.js';
 import jwt from 'jsonwebtoken';
 import orderRouter from './routes/orderRouter.js';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 
 app.use(cors())
@@ -37,7 +39,7 @@ app.use(
     }
 )
 
-mongoose.connect("mongodb+srv://kavindaappuhamy:kavindamongo98@cluster0.hkhcevt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect(process.env.MONGODB_URL)
 .then(()=>{
     console.log("Connected to database");
 }).catch(() => {
